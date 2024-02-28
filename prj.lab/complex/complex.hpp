da4f468ab26ad9f93cf1c693262cb5e103272577
artemmironov243 <145320549+artemmironov243@users.noreply.github.com>
2024-02-28 23:45:43 +0300

    Update complex.hpp

--- prj.lab/complex/complex.hpp
+++ prj.lab/complex/complex.hpp
@@ -5,59 +5,65 @@
 #include <sstream>
 
 struct Complex {
-	Complex() {};
-	Complex(double real);
-	Complex(double real, double imaginary);
+	Complex() = default;
+	explicit Complex(const double real);
+	Complex(const double real, const double imaginary);
 	Complex(const Complex& rhs);
 	Complex& operator=(const Complex& rhs) {
 		re = rhs.re;
 		im = rhs.im;
 		return *this;
 	}
-	~Complex() {};
-	bool operator ==(const Complex& rhs) const { return (re == rhs.re && im == rhs.im); }
-	bool operator != (const Complex& rhs) const { return !operator==(rhs); }
-	Complex& operator+=(const Complex& rhs);
-	Complex& operator+=(const double rhs) { return operator+=(Complex(rhs)); }
-	Complex& operator-=(const Complex& rhs);
-	Complex& operator-=(const double rhs) { return operator-=(Complex(rhs)); }
-	Complex& operator*=(const Complex& rhs);
-	Complex& operator*=(const double rhs) { return operator*=(Complex(rhs)); }
+	~Complex() = default;
+	bool operator ==(const Complex& rhs) const noexcept { return (re == rhs.re && im == rhs.im); }
+	bool operator != (const Complex& rhs) const noexcept { return !operator==(rhs); }
+	Complex& operator+=(const Complex& rhs) noexcept;
+	Complex& operator+=(const double rhs) noexcept { return operator+=(Complex(rhs)); }
+	Complex& operator-=(const Complex& rhs) noexcept;
+	Complex& operator-=(const double rhs) noexcept { return operator-=(Complex(rhs)); }
+	Complex& operator*=(const Complex& rhs) noexcept;
+	Complex& operator*=(const double rhs) noexcept { return operator*=(Complex(rhs)); }
 	Complex& operator/=(const Complex& rhs);
 	Complex& operator/=(const double rhs) { return operator/=(Complex(rhs)); }
-	std::istream& readFrom(std::istream& istrm, Complex& rhs);
+	std::istream& ReadFrom(std::istream& istrm, Complex& rhs) noexcept;
+	srd::istream& WriteTo(std::ostream& ostrm) const noexcept;
 
 	double re{ 0.0 };
 	double im{ 0.0 };
 
-	static const char leftbrace{ '{' };
+	static const char leftBrace{ '{' };
 	static const char separator{ '.' };
-	static const char rightbrace{ '}' };
+	static const char rightBrace{ '}' };
 };
 
-Complex operator+(const Complex& lhs, const Complex& rhs);
-Complex operator+(const Complex& lhs, const double& rhs);
-Complex operator+(const double& lhs, const Complex& rhs);
+Complex operator+(const Complex& lhs, const Complex& rhs) noexcept;
+Complex operator+(const Complex& lhs, const double& rhs) noexcept;
+Complex operator+(const double& lhs, const Complex& rhs) noexcept;
 
-Complex operator-(const Complex& lhs, const Complex& rhs);
-Complex operator-(const Complex& lhs, const double& rhs);
-Complex operator-(const double& lhs, const Complex& rhs);
+Complex operator-(const Complex& lhs, const Complex& rhs) noexcept;
+Complex operator-(const Complex& lhs, const double& rhs) noexcept;
+Complex operator-(const double& lhs, const Complex& rhs) noexcept;
 
-Complex operator*(const Complex& lhs, const Complex& rhs);
-Complex operator*(const Complex& lhs, const double& rhs);
-Complex operator*(const double& lhs, const Complex& rhs);
+Complex operator*(const Complex& lhs, const Complex& rhs) noexcept;
+Complex operator*(const Complex& lhs, const double& rhs) noexcept;
+Complex operator*(const double& lhs, const Complex& rhs) noexcept;
 
 Complex operator/(const Complex& lhs, const Complex& rhs);
 Complex operator/(const Complex& lhs, const double& rhs);
 Complex operator/(const double& lhs, const Complex& rhs);
 
-std::istream& operator >>(std::istream& istrm, Complex& rhs);
-std::ostream& operator <<(std::ostream& ostrm, const Complex& rhs);
-
 bool testing(const std::string& str);
 
 bool operator==(const Complex& lhs, const double& rhs);
 bool operator==(const double& lhs, const Complex& rhs);
+
+inline std::ostream& operator<<(std::ostream& ostrm, const Complex& rhs) noexcept {
+  return rhs.WriteTo(ostrm);
+}
+
+inline std::istream& operator>>(std::istream& istrm, Complex& rhs) noexcept {
+  return rhs.ReadFrom(istrm);
+}
 
 #endif 
 
